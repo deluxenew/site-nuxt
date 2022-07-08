2beaa26e248da0360c00e8d1a932e9788600f7e8
deluxenew <deluxenew@ya.ru>
2022-07-08 21:06:49 +0300

    fix indents

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,35 +1,37 @@
 <template lang="pug">
-  .p-1
-    .title Палень урапвления
-    .p-2.w-100.flex.flex-wrap
-      nuxt-link.p-2.item(tag='div' v-for='item in items' class='w-1/4' :key='item.slug' :to="'admin/' + item.slug")
-        .m-2.p-4.text-4.transition-all(class='bg-[#50d71e] hover:bg-[#50df1e] cursor-[url(https://cdn.custom-cursor.com/db/5388/32/tree-felling-cursor-b.png),_pointer]')
-          | {{item.title}}
-    div
-      .form
-        input.outline(v-model='title' type='text')
-        input.outline(v-model='slug' type='text')
-      .title
-        | Поля
-      .form
-        .list
-          .list-item(v-for='(field, j) in fields' :key='j')
-            div {{field.key}}
-            input.outline(v-model='field.value' type='text')
-            .wrapper
-              .title Свойства
-              .list
-                .list-item(v-for='(prop, i) in field.props' :key='i')
-                  input.outline(v-model='prop.key' type='text')
-                  input.outline(v-model='prop.value' type='text')
-              button(@click='addProp(field)') Добавить свойство
-        button(@click='addField') Добавить Поле
-      button(@click='addCategory')
-        | Добавить
-      button(@click='fetchAllCategories')
-        | Получить всех
-    | Готовый объект полей
-    pre {{fieldsObject}}
+div.p-1
+  div.title Палень урапвления
+  div.p-2.w-100.flex.flex-wrap
+    nuxt-link.p-2.item(
+      tag='div'
+      v-for='item in items'
+      class='w-1/4'
+      :key='item.slug'
+      :to="'admin/' + item.slug"
+    )
+      div.m-2.p-4.text-4.transition-all(class='bg-[#50d71e] hover:bg-[#50df1e] cursor-[url(https://cdn.custom-cursor.com/db/5388/32/tree-felling-cursor-b.png),_pointer]')
+        | {{item.title}}
+  div.form
+    input.outline(v-model='title' type='text')
+    input.outline(v-model='slug' type='text')
+  div.title Поля
+  div.form
+    div.list
+      div.list-item(v-for='(field, j) in fields' :key='j')
+        div {{field.key}}
+        input.outline(v-model='field.value' type='text')
+        div.wrapper
+          div.title Свойства
+          div.list
+            div.list-item(v-for='(prop, i) in field.props' :key='i')
+              input.outline(v-model='prop.key' type='text')
+              input.outline(v-model='prop.value' type='text')
+          button(@click='addProp(field)') Добавить свойство
+    button(@click='addField') Добавить Поле
+  button(@click='addCategory') Добавить
+  button(@click='fetchAllCategories') Получить всех
+  | Готовый объект полей
+  pre {{fieldsObject}}
 
 </template>
 
